907c9bc98df6b8f4d5573fdcd5ba5bea303462c3
Ben Sales <adebsa2401@gmail.com>
2022-10-29 12:34:25 +0100

    feat: Use luxon library to display the date-time below the nav bar

--- modules/dateTime.js
+++ modules/dateTime.js
@@ -1,6 +1,15 @@
+import { DateTime } from '../node_modules/luxon/build/es6/luxon.js';
+
 export default () => {
   setInterval(() => {
-    const current = new Date();
-    document.getElementById('date-time').innerText = current.toLocaleString();
+    const now = DateTime.now();
+    document.getElementById('date-time').innerText = now.toLocaleString({
+      month: 'long',
+      day: 'numeric',
+      year: 'numeric',
+      hour: 'numeric',
+      minute: '2-digit',
+      second: '2-digit',
+    });
   }, 1000);
 };
